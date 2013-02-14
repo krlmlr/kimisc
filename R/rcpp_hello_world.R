ec26b2534b5f59abe3185ff750367937148353fb
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-02-14 16:12:03 +0100

    autogenerated comments for rcpp_hello_world by roxygen

--- R/rcpp_hello_world.R
+++ R/rcpp_hello_world.R
@@ -1,6 +1,11 @@
 #' @useDynLib kimisc
 #' @exportPattern "^[[:alpha:]]+"
 
+#' @docType 'package'
+#' @title Simple function using Rcpp
+#' @examples \dontrun{
+#' rcpp_hello_world()
+#' }
 rcpp_hello_world <-
 function () 
 {
