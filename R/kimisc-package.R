3d60189e7d667a1d416daa396537d8f80db64a2e
muelleki <kirill.mueller@ivt.baug.ethz.ch>
2013-02-14 22:04:12 +0100

    store package and data documentation in one file

--- R/kimisc-package.R
+++ R/kimisc-package.R
@@ -9,3 +9,15 @@
 #' @examples
 #' data(fortytwo)
 NULL
+#' @name fortytwo
+#' @title The Answer to the Ultimate Question of Life, the Universe, and
+#'   Everything
+#' @description This has been calculated by an enormous supercomputer
+#'   over a period of 7.5 million years. Unfortunately no one knows what
+#'   the question is.
+#' @docType data
+#' @usage fortytwo
+#' @format A data frame with 1 row
+#' @source The Hitchhiker's Guide to the Galaxy
+#' @author Douglas Adams
+NULL
