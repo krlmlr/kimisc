f13311a0af04a583e7c2fc7d94c6046f21458bcc
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-07-09 15:36:10 +0200

    prepare project for use with roxygen2 by calling Rd2roxygen('.', 'Template.R')

---
+++ R/Template.R
@@ -0,0 +1,29 @@
+#' What the package does (short line) ~~ package title ~~
+#' 
+#' More about what it does (maybe more than one line) ~~ A concise (1-5 lines)
+#' description of the package ~~
+#' 
+#' \tabular{ll}{ Package: \tab Template\cr Type: \tab Package\cr Version:
+#' \tab 1.0\cr Date: \tab 2013-07-09\cr License: \tab What license is it
+#' under?\cr } ~~ An overview of how to use the package, including the most
+#' important functions ~~
+#' 
+#' @name Template-package
+#' @aliases Template-package Template
+#' @docType package
+#' @author Who wrote it
+#' 
+#' Maintainer: Who to complain to <yourfault@@somewhere.net> ~~ The author
+#' and/or maintainer of the package ~~
+#' @seealso ~~ Optional links to other man pages, e.g. ~~ ~~
+#' \code{\link[<pkg>:<pkg>-package]{<pkg>}} ~~
+#' @references ~~ Literature or other references for background information ~~
+#' @keywords package
+#' @examples
+#' 
+#' ~~ simple examples of the most important functions ~~
+#' 
+NULL
+
+
+
