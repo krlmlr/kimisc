6228cef080e6e1e5b5a2efa48c79477bfcbce337
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-08-27 20:49:55 +0200

    new function nc, similar to nlist

--- pkg/R/nlist.R
+++ pkg/R/nlist.R
@@ -16,6 +16,24 @@
 nlist <- function(...)
   .nworker(list(...), substitute(list(...))[-1])
 
+#' @title Smart named vector
+#' @description This function is a wrapper around \code{\link{c}} that
+#'   assigns names to unnamed arguments based on the unevaluated expression used
+#'   in the call.
+#' @param ... Vector elements, possibly named
+#' @return A named vector.
+#' @seealso \link{c}, \link{nlist}
+#' @examples
+#' a <- 1; b <- 2; c <- 3
+#' nc(a, b, d=c)
+#' nc(mean(c(a, b, c)))
+#' @export
+#' @author Hadley Wickham
+#' @references \url{http://stackoverflow.com/a/5043280/946850},
+#'   \url{http://tolstoy.newcastle.edu.au/R/e9/help/10/03/8392.html}
+nc <- function(...)
+  .nworker(c(...), substitute(list(...))[-1])
+
 #' @title Set the Missing Names in an Object
 #' @description This function is an enhanced version of \code{\link{setNames}}
 #'   in the sense that the elements that already have names are not renamed.
