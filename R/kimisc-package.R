fc2fc678d8867700d0f1bf96a25bd36b4f483ed8
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-09-13 09:32:04 +0200

    Bump version to 0.1.1

--- R/kimisc-package.R
+++ R/kimisc-package.R
@@ -12,8 +12,8 @@
 #' \tabular{ll}{
 #'   Package: \tab kimisc\cr
 #'   Type: \tab Package\cr
-#'   Version: \tab 0.1.0\cr
-#'   Date: \tab 2013-09-12\cr
+#'   Version: \tab 0.1.1\cr
+#'   Date: \tab 2013-09-13\cr
 #'   License: \tab GPL v3\cr
 #' }
 #' @examples
