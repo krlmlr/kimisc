4680ac6f178480194621e7f87a973ae49231cc83
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2013-11-29 11:11:31 +0100

    add tests for thisfile

---
+++ tests/testthat/test-thisfile.R
@@ -0,0 +1,13 @@
+context("thisfile")
+
+test_that("thisfile works with source", {
+  res <- source("scripts/thisfile.R")
+  expect_true(grepl("thisfile.R$", res$value))
+})
+
+test_that("thisfile works with Rscript", {
+  p <- pipe("Rscript scripts/thisfile-cat.R")
+  on.exit(close(p))
+  res <- readLines(p)
+  expect_equal("scripts/thisfile-cat.R", res)
+})
