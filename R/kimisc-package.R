a151bbe1914bd255c0c7902028b462956d3fc66d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-03-19 12:47:22 +0100

    reword title and description of package

--- R/kimisc-package.R
+++ R/kimisc-package.R
@@ -1,9 +1,9 @@
 #' @name kimisc-package
 #' @aliases kimisc
 #' @docType package
-#' @title My first attempt to create an R package
+#' @title Kirill's miscellaneous functions
 #' @description This package is intended to be a collection of more or 
-#'   less useful functions not found in default R or anywhere else.
+#'   less useful functions not found anywhere else.
 #' @author Kirill Müller
 #' @keywords package
 #' @details
