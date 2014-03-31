61d5f14eded6d3623650915c2a3494a6bd89e88a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-03-31 17:05:37 +0200

    update example

--- R/ofactor.R
+++ R/ofactor.R
@@ -8,7 +8,7 @@
 #' @examples
 #' ofactor(3:1)
 #' ofactor(9:12, exclude=11)
-#' ofactor(3:1, levels=1:3)
+#' identical(ofactor(3:1, levels=1:3), factor(3:1))
 #' @export
 ofactor <- function(x = character(), ...) {
   x <- as.character(x)
