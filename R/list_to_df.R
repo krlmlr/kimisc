1b1a1dc4eea361d048d401426c54274874f95e92
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-08 12:24:52 +0200

    new function list_to_df

---
+++ R/list_to_df.R
@@ -0,0 +1,19 @@
+#' Converts a list to a name-value data frame
+#'
+#' This function coerces its input to a list and returns a data frame with as many
+#' rows as there are list items in the input, and two columns
+#' (one for the names, one for the values). If the list is not named, the
+#' natural sequence will be used as item names.
+#'
+#' @export
+list_to_df <- function(list_for_df) {
+  list_for_df <- as.list(list_for_df)
+
+  nm <- names(list_for_df)
+  if (is.null(nm))
+    nm <- seq_along(list_for_df)
+
+  df <- data.frame(name = nm, stringsAsFactors = FALSE)
+  df$value <- unname(list_for_df)
+  df
+}
