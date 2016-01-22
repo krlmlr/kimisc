409c3cc46787736f84fe4c3b72195443ca27c4e9
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-01-22 16:49:24 +0100

    new cut_format()

---
+++ R/cut.R
@@ -0,0 +1,28 @@
+#' Convert Numeric to Factor, with custom formatting
+#'
+#' This is an enhanced version of \code{\link[base]{cut}} that allows a custom
+#' formatting to be applied to the values.
+#'
+#' @seealso \url{http://stackoverflow.com/q/14456371/946850}
+#'
+#' @export
+#' @importFrom utils head tail
+cut_format <- function(x, breaks, include.lowest = FALSE, right = TRUE,
+                       ordered_result = FALSE, ...,
+                       format_fun = format, sep = ", ") {
+  if (right) {
+    ob <- c(include.lowest, rep(FALSE, length(breaks) - 2L))
+    cb <- rep(TRUE, length(breaks) - 1L)
+  } else {
+    ob <- rep(TRUE, length(breaks) - 1L)
+    cb <- c(rep(FALSE, length(breaks) - 2L), include.lowest)
+  }
+
+  ob <- ifelse(ob, "[", "(")
+  cb <- ifelse(cb, "]", ")")
+
+  formatted_breaks <- format_fun(breaks)
+  labels <- paste0(ob, head(formatted_breaks, -1L), sep, tail(formatted_breaks, -1L), cb)
+  cut.default(x = x, breaks = breaks, labels = labels, include.lowest = include.lowest,
+              right = right, ordered_result = ordered_result, ...)
+}
