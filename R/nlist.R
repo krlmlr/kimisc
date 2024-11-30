025c21c707d880d8b61f1574b29cda4cc976fdf3
Kirill Müller <kirill@cynkra.com>
2024-11-30 21:28:23 +0100

    URLs

--- R/nlist.R
+++ R/nlist.R
@@ -13,8 +13,7 @@
 #' nlist(mean(c(a, b, c)))
 #' @export
 #' @author Hadley Wickham
-#' @references [http://stackoverflow.com/a/5043280/946850](),
-#'   [http://tolstoy.newcastle.edu.au/R/e9/help/10/03/8392.html]()
+#' @references [http://stackoverflow.com/a/5043280/946850]()
 #' @family deprecated functions
 #' @name nlist-deprecated
 nlist <- function(...) {
@@ -37,8 +36,7 @@ nlist <- function(...) {
 #' nc(mean(c(a, b, c)))
 #' @export
 #' @author Hadley Wickham
-#' @references [http://stackoverflow.com/a/5043280/946850](),
-#'   [http://tolstoy.newcastle.edu.au/R/e9/help/10/03/8392.html]()
+#' @references [http://stackoverflow.com/a/5043280/946850]()
 #' @family deprecated functions
 #' @name nc-deprecated
 nc <- function(...) {
